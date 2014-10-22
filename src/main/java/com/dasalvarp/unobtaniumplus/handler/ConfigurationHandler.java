f49ec951a4e6dd773cc9287c2a70786d71392cb6
DasAlvarp <alvarogudis@gmail.com>
2014-10-21 23:57:03 -0500

    e
    .sd

---
+++ src/main/java/com/dasalvarp/unobtaniumplus/handler/ConfigurationHandler.java
@@ -0,0 +1,44 @@
+package com.dasalvarp.unobtaniumplus.handler;
+
+import java.io.File;
+
+import com.dasalvarp.unobtaniumplus.reference.Reference;
+
+import cpw.mods.fml.client.event.ConfigChangedEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.common.config.Configuration;
+
+public class ConfigurationHandler
+{
+    public static Configuration configuration;
+    public static boolean explicitMode = false;
+
+    public static void init(File configFile)
+    {
+        // Create the configuration object from the given configuration file
+        if (configuration == null)
+        {
+            configuration = new Configuration(configFile);
+            loadConfiguration();
+        }
+    }
+
+    @SubscribeEvent
+    public void onConfigurationChangedEvent(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.modID.equalsIgnoreCase(Reference.MODID))
+        {
+            loadConfiguration();
+        }
+    }
+
+    private static void loadConfiguration()
+    {
+        explicitMode = configuration.getBoolean("I can curse?", Configuration.CATEGORY_GENERAL, false, "This is an example config value.");
+
+        if (configuration.hasChanged())
+        {
+            configuration.save();
+        }
+    }
+}
--- src/main/java/com/dasalvarp/unobtaniumplus/reference/Reference.java
+++ src/main/java/com/dasalvarp/unobtaniumplus/reference/Reference.java
@@ -5,7 +5,8 @@ public class Reference {
 	
 	public static final String MODID = "UnobtaniumPlus";
 	public static final String MODNAME = "Unobtanium+";
-	public static final String VERSION = "1.7.10-0.0.0a";
+	public static final String VERSION = "1.7.10-0.0.1a";
 	public static final String SERVER_PROXY_CLASS =  "com.dasalvarp.unobtaniumplus.proxy.ServerProxy";
 	public static final String CLIENT_PROXY_CLASS =  "com.dasalvarp.unobtaniumplus.proxy.ClientProxy";
+	public static final String GUI_FACTORY_CLASS = "com.dasalvarp.unobtaniumplus.client.gui.GuiFactory";
 }
