5bafa4c8bdb29350a7f1daa517a455512924c9c1
DasAlvarp <alvarogudis@gmail.com>
2014-10-22 15:45:58 -0500

    to revert
    still have to revert to before linux.

--- src/main/java/com/dasalvarp/unobtaniumplus/UnobtaniumPlus.java
+++ src/main/java/com/dasalvarp/unobtaniumplus/UnobtaniumPlus.java
@@ -2,12 +2,10 @@ package com.dasalvarp.unobtaniumplus;
 
 
 
-import com.dasalvarp.unobtaniumplus.handler.ConfigurationHandler;
+import com.dasalvarp.unobtaniumplus.configuration.ConfigurationHandler;
 import com.dasalvarp.unobtaniumplus.proxy.IProxy;
 import com.dasalvarp.unobtaniumplus.reference.Reference;
-import com.dasalvarp.unobtaniumplus.utility.LogHelper;
 
-import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -16,56 +14,32 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 
 
-@Mod(modid=Reference.MODID, name=Reference.MODNAME, version=Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)//registers it as a mod.
+@Mod(modid=Reference.MODID, name=Reference.MODNAME, version=Reference.VERSION)//registers it as a mod.
 public class UnobtaniumPlus {
 
 	@Mod.Instance(Reference.MODID)
 	public static UnobtaniumPlus instance;//static object that's the mod, completely untouched by man. safe.
 	
-	
-	
-	
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
 	public static IProxy proxy;
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event)//can be whatever I want to call it. Arg is the only important thing.
 	{
 		//load configs
 		ConfigurationHandler.init(event.getSuggestedConfigurationFile());
-		FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
-		LogHelper.info("Pre-Init complete");
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event)
 	{
-		LogHelper.info("Init complete");
+		
 	}
 	
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event)//veeery basic structure of a mod.
 	{
-		LogHelper.info("Post-Init complete");
+		
 	}
 }
 
